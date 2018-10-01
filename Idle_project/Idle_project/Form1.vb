6e875206e55f64ff0690b0953265f6646dd41b8b
kris <kriskruse@hotmail.com>
2018-10-01 15:24:35 +0200

    fs

--- Idle_project/Idle_project/Form1.vb
+++ Idle_project/Idle_project/Form1.vb
@@ -2,6 +2,8 @@
     Dim EXP As Integer = 0
     Dim LVL As Integer = 1
     Dim LVLUpCost As Integer = 0
+    Dim AutoClick1_cost As Integer = 50
+    Dim AutoclickExp = 0
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -28,4 +30,16 @@
         LVLUpCost = Math.Ceiling(LVL * Math.Log10(5 * LVL))
     End Sub
 
+
+    Sub Loop1()
+        While AutoclickExp >= 1
+            EXP += AutoclickExp
+        End While
+    End Sub
+
+    Private Sub AutoClickBuy_Click(sender As Object, e As EventArgs) Handles AutoClickBuy.Click
+        If EXP >= AutoClick1_cost Then
+            AutoclickExp += 1
+        End If
+    End Sub
 End Class
