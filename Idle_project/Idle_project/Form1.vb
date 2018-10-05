162d73b1c236c6c0aaf4b94b92ecc839b3dc8342
TheMineKeeper <minecraftsimon99@gmail.com>
2018-10-05 12:42:37 +0200

    Made the levelcost calc and EXPgain calc more alike so they follow almost the same curve

--- Idle_project/Idle_project/Form1.vb
+++ Idle_project/Idle_project/Form1.vb
@@ -1,20 +1,21 @@
 ﻿Public Class Form1
-    Dim EXP As Integer = 0
-    Dim LVL As Integer = 1
-    Dim LVLUpCost As Integer = 0
-    Dim AutoClick1_cost As Integer = 50
-    Dim AutoclickExp = 0
+    Dim EXP As Integer = 0              'experience, used as currency for leveling up
+    Dim LVL As Integer = 1              'Levels, used to increase amount of experience gained per click/autoclick
+    Dim LVLUpCost As Integer = 0        'Cost for leveling up, calculated based on this calculation: 1 + LVL^(0,25 * LVL)
+    Dim AutoClick1_cost As Integer = 50 'Cost of autoclicker, going to be based on some mathemagics
+    Dim AutoclickExp = 0                'Unknown, Kris can probably explain what it's going to be used for
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub btnMain_Click(sender As Object, e As EventArgs) Handles btnMain.Click
+    Private Sub btnMain_Click(sender As Object, e As EventArgs) Handles btnMain.Click   'activates set functions when a button is clicked
         clickF()
         UpdateLabels()
     End Sub
 
-    Sub clickF()
-        EXP += 1 + Math.Pow(LVL, 0.2 * LVL)
+    Sub clickF()    'Calculates and adds the amount of EXP added per click
+        EXP += 1 + Math.Pow(LVL, 0.2 * LVL) 'the gotten EXP is based on this calculation: 1 + LVL^(0,2 * LVL)
     End Sub
 
     Private Sub BtnLVLUp_Click(sender As Object, e As EventArgs) Handles BtnLVLUp.Click
@@ -28,7 +29,7 @@
     End Sub
 
     Sub LVLUpCostCalc()
-        LVLUpCost = Math.Round(LVL * Math.Pow(LVL, 0.25 * LVL))
+        LVLUpCost = 1 + Math.Pow(LVL, 0.25 * LVL)
     End Sub
 
     Sub UpdateLabels()
