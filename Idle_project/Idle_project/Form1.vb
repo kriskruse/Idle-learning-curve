6f726e4779fd7f828c1ca41ca343e0ad6a667421
TheMineKeeper <minecraftsimon99@gmail.com>
2018-10-06 09:59:06 +0200

    added comments to the newly made code, forget to do that before commiting. Oops

--- Idle_project/Idle_project/Form1.vb
+++ Idle_project/Idle_project/Form1.vb
@@ -2,15 +2,15 @@
     Dim EXP As Integer = 0              'experience, used as currency for leveling up, modifiers for EXP gained include: perception, monsters and (special bonus for endgame)
     Dim LVL As Integer = 1              'Levels, used to increase amount of experience gained per click/autoclick
     Dim DarkVisionLVL As Integer = 0    'Levels in skill: Darkvision, Used to increase EXP gained and to unlock next part of the story.
-    Dim DarkVisionCost As Integer = 1
-    Dim DarkVisionEXP As Integer = 0
+    Dim DarkVisionCost As Integer = 1   'Cost to level up the skill darkvision. (Might make this an overall skill EXP thing)
+    Dim DarkVisionEXP As Integer = 0    'Experience used to level up Dark Vision skill.
     Dim LVLUpCost As Integer = 5        'Cost for leveling up, calculated based on this calculation: 1 + LVL^(0,25 * LVL)
     Dim AutoClick1_cost As Integer = 50 'Cost of autoclicker, going to be based on some mathemagics
     Dim AutoclickExp = 0                'Unknown, Kris can probably explain what it's going to be used for
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        LVLUpCostCalc()
-        UpdateLabels()
+        LVLUpCostCalc()                 'calculates the cost of every levelup button at startup
+        UpdateLabels()                  'updates every label on startup.
     End Sub
 
     Private Sub btnMain_Click(sender As Object, e As EventArgs) Handles btnMain.Click   'activates set functions when a button is clicked
@@ -18,26 +18,26 @@
         UpdateLabels()
     End Sub
 
-    Sub clickF()    'Calculates and adds the amount of EXP added per click
+    Sub clickF()                        'Calculates and adds the amount of EXP added per click
         EXP += DarkVisionLVL + Math.Pow(LVL, 0.1 * LVL) 'the gotten EXP is based on this calculation: 1 + LVL^(0,1 * LVL)
         DarkVisionEXP += Math.Pow(DarkVisionLVL, 0.05 * DarkVisionLVL)
     End Sub
 
     Private Sub BtnLVLUp_Click(sender As Object, e As EventArgs) Handles BtnLVLUp.Click 'Button to activate different functions and calculations for leveling up
-        If EXP >= LVLUpCost Then    'checks whether or not the player has enough EXP
-            EXP -= LVLUpCost        'starts the levelup process by lowering EXP by the cost of leveling
-            LVL += 1                'Increases the level.
-            LVLUpCostCalc()         'Calculates what the next level costs.
-            UpdateLabels()          'Updates all the labels visible to player.
+        If EXP >= LVLUpCost Then        'checks whether or not the player has enough EXP
+            EXP -= LVLUpCost            'starts the levelup process by lowering EXP by the cost of leveling
+            LVL += 1                    'Increases the level.
+            LVLUpCostCalc()             'Calculates what the next level costs.
+            UpdateLabels()              'Updates all the labels visible to player.
         End If
     End Sub
 
-    Sub LVLUpCostCalc()             'Calculates what the next level costs.
+    Sub LVLUpCostCalc()                 'Calculates what the next level costs.
         LVLUpCost = 1 + Math.Pow(LVL, 0.15 * LVL)   'sets the levelcost, based on the calculation: 1 + LVL^(0,15 * LVL)
         DarkVisionCost = Math.Pow(DarkVisionLVL, 0.1 * DarkVisionLVL)
     End Sub
 
-    Sub UpdateLabels()              'Updates the Labels by setting the text of them equal to the actual values.
+    Sub UpdateLabels()                  'Updates the Labels by setting the text of them equal to the actual values.
         LblEXP.Text = EXP
         LblLVL.Text = LVL
         lblLVLCost.Text = LVLUpCost
@@ -45,7 +45,7 @@
         LblDarkVisionEXP.Text = DarkVisionEXP
     End Sub
 
-    Private Sub BtnDarkVision_Click(sender As Object, e As EventArgs) Handles BtnDarkVision.Click
+    Private Sub BtnDarkVision_Click(sender As Object, e As EventArgs) Handles BtnDarkVision.Click   'starts the level up procedure for darkvision skill.
         If DarkVisionEXP >= DarkVisionCost Then
             DarkVisionLVL += 1
             DarkVisionEXP -= DarkVisionCost
