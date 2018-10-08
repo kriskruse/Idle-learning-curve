b10ef66fb53b77301b9e0f6c9915703ed609a4f1
kris <kriskruse@hotmail.com>
2018-10-08 13:09:50 +0200

    Autoclicker fixed

--- Idle_project/Idle_project/Form1.Designer.vb
+++ Idle_project/Idle_project/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.BtnMain = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -41,14 +42,16 @@ Partial Class Form1
         Me.Label6 = New System.Windows.Forms.Label()
         Me.LblDarkVisionCost = New System.Windows.Forms.Label()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.TimerAutoclick = New System.Windows.Forms.Timer(Me.components)
+        Me.lblAutoClickcost = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'BtnMain
         '
-        Me.BtnMain.Location = New System.Drawing.Point(9, 10)
-        Me.BtnMain.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnMain.Location = New System.Drawing.Point(12, 12)
+        Me.BtnMain.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BtnMain.Name = "BtnMain"
-        Me.BtnMain.Size = New System.Drawing.Size(251, 449)
+        Me.BtnMain.Size = New System.Drawing.Size(335, 553)
         Me.BtnMain.TabIndex = 0
         Me.BtnMain.Text = "CLICK ME TO GRIND FOR EXP"
         Me.BtnMain.UseVisualStyleBackColor = True
@@ -57,9 +60,10 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(265, 10)
+        Me.Label1.Location = New System.Drawing.Point(353, 12)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(28, 13)
+        Me.Label1.Size = New System.Drawing.Size(35, 17)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "EXP"
         '
@@ -67,9 +71,10 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(339, 335)
+        Me.Label2.Location = New System.Drawing.Point(452, 412)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(26, 13)
+        Me.Label2.Size = New System.Drawing.Size(33, 17)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "LVL"
         '
@@ -77,9 +82,10 @@ Partial Class Form1
         '
         Me.LblEXP.AutoSize = True
         Me.LblEXP.ForeColor = System.Drawing.Color.White
-        Me.LblEXP.Location = New System.Drawing.Point(299, 10)
+        Me.LblEXP.Location = New System.Drawing.Point(399, 12)
+        Me.LblEXP.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblEXP.Name = "LblEXP"
-        Me.LblEXP.Size = New System.Drawing.Size(13, 13)
+        Me.LblEXP.Size = New System.Drawing.Size(16, 17)
         Me.LblEXP.TabIndex = 3
         Me.LblEXP.Text = "0"
         '
@@ -87,17 +93,19 @@ Partial Class Form1
         '
         Me.LblLVL.AutoSize = True
         Me.LblLVL.ForeColor = System.Drawing.Color.White
-        Me.LblLVL.Location = New System.Drawing.Point(339, 349)
+        Me.LblLVL.Location = New System.Drawing.Point(452, 430)
+        Me.LblLVL.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblLVL.Name = "LblLVL"
-        Me.LblLVL.Size = New System.Drawing.Size(13, 13)
+        Me.LblLVL.Size = New System.Drawing.Size(16, 17)
         Me.LblLVL.TabIndex = 4
         Me.LblLVL.Text = "1"
         '
         'BtnLVLUp
         '
-        Me.BtnLVLUp.Location = New System.Drawing.Point(264, 365)
+        Me.BtnLVLUp.Location = New System.Drawing.Point(352, 449)
+        Me.BtnLVLUp.Margin = New System.Windows.Forms.Padding(4)
         Me.BtnLVLUp.Name = "BtnLVLUp"
-        Me.BtnLVLUp.Size = New System.Drawing.Size(143, 57)
+        Me.BtnLVLUp.Size = New System.Drawing.Size(191, 70)
         Me.BtnLVLUp.TabIndex = 5
         Me.BtnLVLUp.Text = "Level up"
         Me.BtnLVLUp.UseVisualStyleBackColor = True
@@ -106,9 +114,10 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(265, 336)
+        Me.Label3.Location = New System.Drawing.Point(353, 414)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(28, 13)
+        Me.Label3.Size = New System.Drawing.Size(36, 17)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Cost"
         '
@@ -116,18 +125,19 @@ Partial Class Form1
         '
         Me.lblLVLCost.AutoSize = True
         Me.lblLVLCost.ForeColor = System.Drawing.Color.White
-        Me.lblLVLCost.Location = New System.Drawing.Point(265, 349)
+        Me.lblLVLCost.Location = New System.Drawing.Point(353, 430)
+        Me.lblLVLCost.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblLVLCost.Name = "lblLVLCost"
-        Me.lblLVLCost.Size = New System.Drawing.Size(13, 13)
+        Me.lblLVLCost.Size = New System.Drawing.Size(16, 17)
         Me.lblLVLCost.TabIndex = 7
         Me.lblLVLCost.Text = "0"
         '
         'AutoClickBuy
         '
-        Me.AutoClickBuy.Location = New System.Drawing.Point(264, 463)
-        Me.AutoClickBuy.Margin = New System.Windows.Forms.Padding(2)
+        Me.AutoClickBuy.Location = New System.Drawing.Point(352, 570)
+        Me.AutoClickBuy.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.AutoClickBuy.Name = "AutoClickBuy"
-        Me.AutoClickBuy.Size = New System.Drawing.Size(144, 57)
+        Me.AutoClickBuy.Size = New System.Drawing.Size(192, 70)
         Me.AutoClickBuy.TabIndex = 8
         Me.AutoClickBuy.Text = "Auto Grinder"
         Me.AutoClickBuy.UseVisualStyleBackColor = True
@@ -137,18 +147,20 @@ Partial Class Form1
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(629, 10)
+        Me.Label4.Location = New System.Drawing.Point(839, 12)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
         Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label4.Size = New System.Drawing.Size(158, 13)
+        Me.Label4.Size = New System.Drawing.Size(208, 17)
         Me.Label4.TabIndex = 9
         Me.Label4.Text = "What skill do you want to grind?"
         '
         'BtnDarkVision
         '
-        Me.BtnDarkVision.Location = New System.Drawing.Point(714, 42)
+        Me.BtnDarkVision.Location = New System.Drawing.Point(952, 52)
+        Me.BtnDarkVision.Margin = New System.Windows.Forms.Padding(4)
         Me.BtnDarkVision.Name = "BtnDarkVision"
-        Me.BtnDarkVision.Size = New System.Drawing.Size(75, 23)
+        Me.BtnDarkVision.Size = New System.Drawing.Size(100, 28)
         Me.BtnDarkVision.TabIndex = 10
         Me.BtnDarkVision.Text = "Dark Vision"
         Me.BtnDarkVision.UseVisualStyleBackColor = True
@@ -157,9 +169,10 @@ Partial Class Form1
         '
         Me.Label5.AutoSize = True
         Me.Label5.ForeColor = System.Drawing.Color.White
-        Me.Label5.Location = New System.Drawing.Point(486, 10)
+        Me.Label5.Location = New System.Drawing.Point(648, 12)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.Size = New System.Drawing.Size(76, 17)
         Me.Label5.TabIndex = 11
         Me.Label5.Text = "Skill Points"
         '
@@ -167,35 +180,39 @@ Partial Class Form1
         '
         Me.LblSkillPoints.AutoSize = True
         Me.LblSkillPoints.ForeColor = System.Drawing.Color.White
-        Me.LblSkillPoints.Location = New System.Drawing.Point(545, 10)
+        Me.LblSkillPoints.Location = New System.Drawing.Point(727, 12)
+        Me.LblSkillPoints.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblSkillPoints.Name = "LblSkillPoints"
-        Me.LblSkillPoints.Size = New System.Drawing.Size(13, 13)
+        Me.LblSkillPoints.Size = New System.Drawing.Size(16, 17)
         Me.LblSkillPoints.TabIndex = 12
         Me.LblSkillPoints.Text = "0"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(489, 26)
+        Me.Button1.Location = New System.Drawing.Point(652, 32)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(219, 306)
+        Me.Button1.Size = New System.Drawing.Size(292, 377)
         Me.Button1.TabIndex = 13
         Me.Button1.Text = "*TEMP* Fighting area"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(413, 336)
+        Me.Button2.Location = New System.Drawing.Point(551, 414)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(295, 184)
+        Me.Button2.Size = New System.Drawing.Size(393, 226)
         Me.Button2.TabIndex = 14
         Me.Button2.Text = "*TEMP* STAT OVERLAY"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(264, 26)
+        Me.Button3.Location = New System.Drawing.Point(352, 32)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(219, 306)
+        Me.Button3.Size = New System.Drawing.Size(292, 377)
         Me.Button3.TabIndex = 15
         Me.Button3.Text = "*TEMP* Story area"
         Me.Button3.UseVisualStyleBackColor = True
@@ -204,9 +221,10 @@ Partial Class Form1
         '
         Me.Label6.AutoSize = True
         Me.Label6.ForeColor = System.Drawing.Color.White
-        Me.Label6.Location = New System.Drawing.Point(711, 26)
+        Me.Label6.Location = New System.Drawing.Point(948, 32)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.Size = New System.Drawing.Size(80, 17)
         Me.Label6.TabIndex = 16
         Me.Label6.Text = " Vision cost"
         '
@@ -214,28 +232,44 @@ Partial Class Form1
         '
         Me.LblDarkVisionCost.AutoSize = True
         Me.LblDarkVisionCost.ForeColor = System.Drawing.Color.White
-        Me.LblDarkVisionCost.Location = New System.Drawing.Point(774, 26)
+        Me.LblDarkVisionCost.Location = New System.Drawing.Point(1032, 32)
+        Me.LblDarkVisionCost.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblDarkVisionCost.Name = "LblDarkVisionCost"
-        Me.LblDarkVisionCost.Size = New System.Drawing.Size(13, 13)
+        Me.LblDarkVisionCost.Size = New System.Drawing.Size(16, 17)
         Me.LblDarkVisionCost.TabIndex = 17
         Me.LblDarkVisionCost.Text = "0"
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(9, 463)
-        Me.Button4.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button4.Location = New System.Drawing.Point(12, 570)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(251, 57)
+        Me.Button4.Size = New System.Drawing.Size(335, 70)
         Me.Button4.TabIndex = 18
         Me.Button4.Text = "*TEMP* Rebirth"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'TimerAutoclick
+        '
+        Me.TimerAutoclick.Interval = 1
+        '
+        'lblAutoClickcost
+        '
+        Me.lblAutoClickcost.AutoSize = True
+        Me.lblAutoClickcost.ForeColor = System.Drawing.Color.White
+        Me.lblAutoClickcost.Location = New System.Drawing.Point(353, 548)
+        Me.lblAutoClickcost.Name = "lblAutoClickcost"
+        Me.lblAutoClickcost.Size = New System.Drawing.Size(51, 17)
+        Me.lblAutoClickcost.TabIndex = 19
+        Me.lblAutoClickcost.Text = "Label7"
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(799, 531)
+        Me.ClientSize = New System.Drawing.Size(1065, 654)
+        Me.Controls.Add(Me.lblAutoClickcost)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.LblDarkVisionCost)
         Me.Controls.Add(Me.Label6)
@@ -255,7 +289,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.BtnMain)
-        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "You find yourself in a dark cave"
         Me.ResumeLayout(False)
@@ -282,4 +316,6 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents LblDarkVisionCost As Label
     Friend WithEvents Button4 As Button
+    Friend WithEvents TimerAutoclick As Timer
+    Friend WithEvents lblAutoClickcost As Label
 End Class
--- Idle_project/Idle_project/Form1.vb
+++ Idle_project/Idle_project/Form1.vb
@@ -1,19 +1,21 @@
 ﻿Public Class Form1
-    Dim EXP As Integer = 0              'experience, used as currency for leveling up, modifiers for EXP gained include: perception, monsters and (special bonus for endgame)
+    Dim EXP As Double = 0              'experience, used as currency for leveling up, modifiers for EXP gained include: perception, monsters and (special bonus for endgame)
     Dim LVL As Integer = 1              'Levels, used to increase amount of experience gained per click/autoclick
     Dim DarkVisionLVL As Integer = 0    'Levels in skill: Darkvision, Used to increase EXP gained and to unlock next part of the story.
     Dim DarkVisionCost As Integer = 1   'Cost to level up the skill darkvision. (Might make this an overall skill EXP thing)
     Dim SkillPoints As Integer = 0      'Skill points, used to level up all skills.
     Dim LVLUpCost As Integer = 5        'Cost for leveling up, calculated based on this calculation: 1 + LVL^(0,25 * LVL)
-    Dim AutoClick1_cost As Integer = 50 'Cost of autoclicker, going to be based on some mathemagics
-    Dim AutoclickExp = 0                'Unknown, Kris can probably explain what it's going to be used for
+    Dim AutoClick1_cost As Double = 50 'Cost of autoclicker, going to be based on some mathemagics
+    Dim AutoClickExp As Double = 0
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LVLUpCostCalc()                 'calculates the cost of every levelup button at startup
         UpdateLabels()                  'updates every label on startup.
+        TimerAutoclick.Enabled = False
     End Sub
 
-    Private Sub BtnMain_Click(sender As Object, e As EventArgs) Handles BtnMain.Click   'activates set functions when a button is clicked
+    Private Sub BtnMain_Click(sender As Object, e As EventArgs) Handles btnMain.Click   'activates set functions when a button is clicked
         ClickF()
         UpdateLabels()
     End Sub
@@ -41,11 +43,12 @@
     End Sub
 
     Sub UpdateLabels()                  'Updates the Labels by setting the text of them equal to the actual values.
-        LblEXP.Text = EXP
+        LblEXP.Text = Math.Round(EXP)
         LblLVL.Text = LVL
         lblLVLCost.Text = LVLUpCost
         LblDarkVisionCost.Text = DarkVisionCost
         LblSkillPoints.Text = SkillPoints
+        lblAutoClickcost.Text = Math.Round(AutoClick1_cost)
     End Sub
 
     Private Sub BtnDarkVision_Click(sender As Object, e As EventArgs) Handles BtnDarkVision.Click   'starts the level up procedure for darkvision skill.
@@ -62,17 +65,19 @@
     ' I have no Idea past this point
     ' I have no Idea past this point
     ' I have no Idea past this point
+    ' Niether has Kris
 
-    Sub Loop1()
-        While AutoclickExp >= 1
-            EXP += AutoclickExp
-        End While
-    End Sub
-
-    Private Sub AutoClickBuy_Click(sender As Object, e As EventArgs) Handles AutoClickBuy.Click
+    Private Sub AutoClickBuy_Click(sender As Object, e As EventArgs) Handles AutoClickBuy.Click 'This is autoclicker Buch no cross
         If EXP >= AutoClick1_cost Then
-            AutoclickExp += 1
+            EXP -= AutoClick1_cost
+            TimerAutoclick.Start()
+            AutoClickExp += 0.01
+            AutoClick1_cost += Math.Pow(AutoClickExp * 100.2, 1.2 * AutoClickExp * 10)
         End If
     End Sub
 
+    Private Sub TimerAutoclick_Tick(sender As Object, e As EventArgs) Handles TimerAutoclick.Tick
+        EXP += AutoclickExp
+        UpdateLabels()
+    End Sub
 End Class
